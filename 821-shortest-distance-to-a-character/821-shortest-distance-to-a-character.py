2f177fd1b6bfd8994ec358674e6ba13b375ea656
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-06-14 21:19:59 +0100

    Time: 70 ms (55.12%), Space: 14 MB (55.22%) - LeetHub

--- 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.py
@@ -3,13 +3,22 @@ class Solution:
         
         n=len(s)
         ans=[]
-        for slow in range(n):
-            fast=0
-            mindis=10**4+1
-            while fast<n:
-                if s[fast]==c:
-                    mindis=min(mindis,abs(fast-slow))
-                fast+=1
-            ans.append(mindis)
-                
+        ind=set()
+        for i in range(n):
+            if s[i]==c:
+                ind.add(i)
+        for i in range(n):
+            ans.append(min(abs(i-k) for k in ind))
+            
         return ans
+#         for slow in range(n):
+#             fast=0
+#             mindis=10**4+1
+#             while fast<n:
+#                 if s[fast]==c:
+#                     mindis=min(mindis,abs(fast-slow))
+#                 fast+=1
+#             ans.append(mindis)
+                
+#         return ans
+    
