a45fd88297756e8603bf79568bf9cd83f4346ba2
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-07-16 00:59:10 +0100

    Time: 45 ms (82.17%), Space: 14.5 MB (26.20%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -17,28 +17,12 @@ class Solution:
         values={}#in the dictionnary, we associate to every integer value the newly created node with the same value and an empty list of neighbors, to be populated later
         visited=set()
         while q:
-            print("q=",end=" ")
-            for i in q:
-                
-                print(i.val,end=" ")
-            print()
             current=q.popleft()
-            print("after pop q=",end=" ")
-            for i in q:
-                
-                print(i.val,end=" ")
-            print()
-            print("current :",current.val)
-            
-            print("visited :",visited)
-            
-            print(values)
             if current.val not in values:
                     node=Node(current.val)
                     values[current.val]=node
             if current.val not in visited:
                 for n in current.neighbors:
-                    print("value :",n.val)
                     if n.val not in values:
                         node=Node(n.val,[])
                         values[n.val]=node
@@ -49,13 +33,3 @@ class Solution:
                     
         return values[1]
     
-    
-
-# q=[(1,[3])]
-# values={1:(1,[])}
-# visited=()
-
-# current=(1,[3]) => q=[]
-# visited=(1)
-# values={1:(1,[(3,[])]),3:(3,[])}
-# q=
