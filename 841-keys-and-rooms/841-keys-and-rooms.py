3510a9db943b9a3d433793374d1e29fcf0e5c0a3
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-07-16 01:27:02 +0100

    Time: 406 ms (5.20%), Space: 14.9 MB (11.40%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.py
@@ -0,0 +1,21 @@
+from collections import deque
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        q=deque()
+        n=len(rooms)
+        q.append(0)
+        visited=set()
+        visitable=set(rooms[0])
+        visitable.add(0)
+        while q:
+            current=q.popleft()
+            visited.add(current)
+            for i in rooms[current]:
+                if i not in visited:
+                    q.append(i)
+            if current in visitable:
+                for i in rooms[current]:
+                    visitable.add(i)
+            print(visitable)
+        
+        return len(visitable)==n
