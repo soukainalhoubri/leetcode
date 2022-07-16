de973dd780f974f8c483ea478fa6a1c4d59f7220
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-07-16 16:23:18 +0100

    Time: 861 ms (77.55%), Space: 19.1 MB (27.98%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        #I should create a hashtable to contain the citizens who trust the judge
+        #if it the citizen trusts someone else, they should be removed from  the hashtable
+        #we test the condidates, we return the element which is trusted by n-1
+        #if there is no element which is trusted by n-1, we return -1
+        
+        trust_count={i:0 for i in range(1,n+1)}
+        for i in trust:
+            trust_count[i[0]]=-1# the judge trusts no one
+            if trust_count[i[1]]!=-1:
+                trust_count[i[1]]+=1
+        
+        for i in trust_count:
+            if trust_count[i]==n-1:
+                return i
+        return -1
