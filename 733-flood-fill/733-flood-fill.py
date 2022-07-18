5fc23e0fb216af4d17206513abbd2eb2d1c46777
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-07-19 00:05:28 +0100

    Time: 150 ms (15.28%), Space: 14.2 MB (38.09%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,11 +1,15 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        rows, cols, orig_color = len(image), len(image[0]), image[sr][sc]
-        def traverse(row, col):
-            if (not (0 <= row < rows and 0 <= col < cols)) or image[row][col] != orig_color:
+        R=len(image)
+        C=len(image[0])
+        original=image[sr][sc]
+        def dfs(r,c):
+            if(not(0<=r<R and 0<=c<C))or image[r][c]!=original:
                 return
-            image[row][col] = color
-            [traverse(row + x, col + y) for (x, y) in ((0, 1), (1, 0), (0, -1), (-1, 0))]
-        if orig_color != color:
-            traverse(sr, sc)
+            image[r][c]=color
+            [dfs(r+x,c+y) for (x,y) in [(1,0),(-1,0),(0,1),(0,-1)]]
+        if original!=color:
+            dfs(sr,sc)
         return image
+    
+    
