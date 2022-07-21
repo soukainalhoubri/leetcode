36ccb680b522c7f4579674065caf254cfd37c3c2
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-07-21 18:41:15 +0100

    Time: 280 ms (25.28%), Space: 15.5 MB (89.12%) - LeetHub

--- 690-employee-importance/690-employee-importance.py
+++ 690-employee-importance/690-employee-importance.py
@@ -16,9 +16,6 @@ class Solution:
         #we will use a dictionnary: creating an adjaceny list
         
         importance={i.id:[i.importance,i.subordinates] for i in employees}#O(V) T&S
-        # visited=set()#O(V) 
-        # visitable=set()#O(V) 
-        # visitable.add(id)
         q=deque()#O(V) 
         q.append(id)
         answer=0#O(1) 
@@ -30,32 +27,6 @@ class Solution:
             
         return answer
         
-        
-        
-        
-        # while q: #O(V+E)
-        #     current=q.popleft()
-        #     visited.add(current)
-        #     for sub in importance[current][1]:
-        #         if current in visitable:
-        #             visitable.add(sub)
-        #         if sub not in visited:
-        #             q.append(sub)
-        # for i in visitable: #O(V)
-        #     answer+=importance[i][0]
-        # return answer
-  
-
-
-
-
-
-#Time complexity:
-#let V = len(employees)= number of vertices & E= number of edges
-# T=O(V)+O(V)+O(V+E)
-
-#space complexity:
-# S=O(V)
 
 
 
