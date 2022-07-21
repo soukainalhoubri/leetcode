2d756445cdba044781dba09d26ed3d92b23fe2ca
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-07-21 18:37:48 +0100

    Time: 164 ms (83.76%), Space: 15.6 MB (58.83%) - LeetHub

--- 690-employee-importance/690-employee-importance.py
+++ 690-employee-importance/690-employee-importance.py
@@ -11,29 +11,52 @@ from collections import deque
 
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        visited=set()
-        q=deque()
-        q.append
         
         #we first need to store the items in a way to make access to them easier by ID
-        #we will use a dictionnary
+        #we will use a dictionnary: creating an adjaceny list
         
-        importance={i.id:[i.importance,i.subordinates] for i in employees}
-        visited=set()
-        visitable=set()
+        importance={i.id:[i.importance,i.subordinates] for i in employees}#O(V) T&S
+        visited=set()#O(V) 
+        visitable=set()#O(V) 
         visitable.add(id)
-        q=deque()
+        q=deque()#O(V) 
         q.append(id)
-        answer=0
+        answer=0#O(1) 
         while q:
             current=q.popleft()
-            visited.add(current)
-            for sub in importance[current][1]:
-                if current in visitable:
-                    visitable.add(sub)
-                if sub not in visited:
-                    q.append(sub)
-        for i in visitable:
-            answer+=importance[i][0]
+            answer+=importance[current][0]
+            for i in importance[current][1]:
+                q.append(i)
+            
         return answer
+        
+        
+        
+        
+        # while q: #O(V+E)
+        #     current=q.popleft()
+        #     visited.add(current)
+        #     for sub in importance[current][1]:
+        #         if current in visitable:
+        #             visitable.add(sub)
+        #         if sub not in visited:
+        #             q.append(sub)
+        # for i in visitable: #O(V)
+        #     answer+=importance[i][0]
+        # return answer
+  
+
+
+
+
+
+#Time complexity:
+#let V = len(employees)= number of vertices & E= number of edges
+# T=O(V)+O(V)+O(V+E)
+
+#space complexity:
+# S=O(V)
+
+
+
             
