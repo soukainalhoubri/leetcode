8d4f12555fb00628622cf0e3086183ab8d373c4b
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-07-28 13:45:08 +0100

    Time: 351 ms (19.31%), Space: 14.3 MB (91.76%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,100 +1,25 @@
 from collections import deque
-
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        #first of all, we should list the edges:
-        color={i:-1 for i in range(len(graph))}
+        #I need to keep track of all of the node colors
+        #I do BFS, and change color
+        #there is no mention that the graph can't be disconnected, se we assume it can
+        colors={i:-1 for i in range(len(graph))}
         n=len(graph)
         visited=set()
-        for i in range(n):
-            if color[i]==-1:
-                color[i]=1
+        colors[0]=1
+        for i in range(len(graph)):
             q=deque()
             q.append(i)
-            visited=set()
             while q:
                 current=q.popleft()
-                visited.add(current)
+                visited.add(i)
                 for adj in graph[current]:
-                    if color[adj]==-1:
-                        color[adj]=1-color[current]
-                    elif color[adj]==color[current]:
+                    if colors[adj]==-1:
+                        colors[adj]=1-colors[current]
+                    elif colors[adj]==colors[current]:
                         return False
-                    if adj not in visited:
-                        q.append(adj)
             if len(visited)==n:
                 return True
-        return True
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         color={i:-1 for i in range(len(graph))}
-#         color[0]=1
-#         n=len(graph)
-#         q=deque()
-#         q.append(0)
-#         visited=set()
-#         while q:
-#             current=q.popleft()
-#             print("current =",current)
-#             visited.add(current)
-#             count=0
-#             for adj in graph[current]:
-#                 print("adj of",current,"=",adj)
-#                 if color[adj]!=-1 and color[current]==color[adj]:
-#                     print("got into first if")
-#                     return False
-#                 elif color[adj]==-1 :
-#                     print("color[adj]=",color[adj])
-#                     print("got into second if")
-#                     color[adj]=1-color[current]
-                    
-#                 if adj not in visited:
-#                     q.append(adj)
-#                     count+=1
-#             if count==0 and not q and len(visited)!=n:
-#                 return False
-                
-                    
-#             print(color)
             
-#         return True
-
-    
-#     #testing:
-    
-
-
-    
-# neighbors=[0,2]
-# while loop:
-#     current=1
-#     q=[2,3]
-#     visited=(0,1)
-#     neighbors loop:
-#         color={0:1,1:0,2:0,3:0}
