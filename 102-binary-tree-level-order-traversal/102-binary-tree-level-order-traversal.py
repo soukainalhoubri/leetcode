ef29bc5b265f6a1687d3b04b7eb424b2b1b2630e
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-08-19 21:59:24 +0100

    Time: 41 ms (83.66%), Space: 14.1 MB (85.22%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -4,51 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        " a function to calculate the height of a tree"
-        ans, level = [], [root]
-        while root and level:
-            ans.append([node.val for node in level])
-            LRpair = [(node.left, node.right) for node in level]
-            level = [leaf for LR in LRpair for leaf in LR if leaf]
-        return ans
-#         def current_level(root,level):
-#             if not root:
-#                 return []
-#             ans=[]
-#             if level==1:
-#                 ans.append(root.val)
-#             elif level>1:
-#                 return current_level(root.left,level-1)
-#                 return current_level(root.right,level-1)
-#             return ans
-            
-#         def height(node):
-#             if node is None:
-#                 return 0
-#             left_height=1+height(node.left)
-#             right_height=1+height(node.right)
-            
-#             # if left_height>right_height:
-#             #     return left_height+1
-#             # if left_height<right_height:
-#             #     return right_height+1
-#             return max(left_height,right_height)
-#         if not root:
-#             return []
-#         ans=[]
-#         h=height(root)
-#         for i in range(1,h+1):
-#             ans.append(current_level(root,i))
-
-            
-            
+        if not root:
+            return []
         
-        
-        
-        return ans
-        
+        q=deque()
+        q.append(root)
+        answer=[]
+        while q:
+            level_length=len(q)
+            current_level=[]
+            for _ in range(level_length):
+                current=q.popleft()
+                current_level.append(current.val)
+                if current.left:
+                    q.append(current.left)
+                if current.right:
+                    q.append(current.right)
+            answer.append(current_level)
+        return answer
         
             
