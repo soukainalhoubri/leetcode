60d199233b8fcdda6cb081ccd7a348bd686b791e
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-09-27 14:32:37 +0100

    Time: 139 ms (25.78%), Space: 14.9 MB (13.01%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.py
+++ 841-keys-and-rooms/841-keys-and-rooms.py
@@ -5,12 +5,11 @@ class Solution:
         #I will start with node 0, and then visit all of its neighbors, I keep track of visited nodes and so on.
         #I will use a queue to enqueue the node to be proccessed, if it gets emptied without visiting all of the nodes, this means I can't visit all of the nodes
         visited=set()
-        q=deque()
-        q.append(0)
-        while q:
-            current=q.popleft()
-            visited.add(current)
-            for room in rooms[current]:
-                if room not in visited:
-                    q.append(room)
+        visited.add(0)
+        def dfs(room):
+            for neighbor in rooms[room]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    dfs(neighbor)
+        dfs(0)
         return len(visited)==len(rooms)
