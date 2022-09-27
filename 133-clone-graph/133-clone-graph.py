80049f3c569449a02d3c4506f6fe65783cc3fee2
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-09-27 13:38:48 +0100

    Time: 57 ms (64.09%), Space: 14.4 MB (77.39%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -5,31 +5,56 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-from collections import deque
+
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        #we first deal with the edge case:
-        if not node:
-            return node
-        
-        q=deque()
-        q.append(node)
-        values={}#in the dictionnary, we associate to every integer value the newly created node with the same value and an empty list of neighbors, to be populated later
-        visited=set()
-        while q:
-            current=q.popleft()
-            if current.val not in values:
-                    node=Node(current.val)
-                    values[current.val]=node
-            if current.val not in visited:
-                for n in current.neighbors:
-                    if n.val not in values:
-                        node=Node(n.val,[])
-                        values[n.val]=node
-                    values[current.val].neighbors.append(values[n.val])
-                    if n.val not in visited:
-                        q.append(n)
-            visited.add(current.val)
+        #I need to clone my node, and I will keep track of its copy to return it.
+        def dfs(node,copies):
+            for neighbor in node.neighbors:
+                if neighbor not in copies:
+                    newNode=Node(neighbor.val)
+                    copies[neighbor]=newNode
+                    copies[node].neighbors.append(newNode)
+                    dfs(neighbor,copies)
+                else:
+                    copies[node].neighbors.append(copies[neighbor])
                     
-        return values[1]
-    
+        if not node:
+            return None
+        newGraph = Node(node.val)
+        copies={node:newGraph}
+        dfs(node,copies)
+        return newGraph
+        #for each node, I will make a new copy.
+        #I need to populate its neighbors
+        #what I wanna make sure of is that, I keep the same created nodes, and create links between them.
+        #node1 --> (node2,node3)--> node2-->node1 --> node3-->node1
+        
+        #to do so, I need to keep trak of the nodes I have already copied, and their copies: hashtable
+        
+        #example
+        
+#         (1,[2,4]) (2,[1,3]) (3,[2,4]) (4,[1,3])
+        
+#         (1',[2',4'])(2',[1',3']) (3',[2',4']) (4',[1',3'])
+#         {1:1',2:2'}
+         
+         #1->(2->1)-->3
+         #iterate over its neighbors
+         #2 --> not in the dict, so I create a copy for it
+         #I add it to current main node's neighbors
+         #1--> in dict, so I add it's copy as a neighbor for the current working node : 2
+         #3--> not in dict, we create a copy for it, and add to 2' neighs
+         # we go through 3neighs
+         #2 is in the dict, so we add itscopy to our node's neighs
+         #4 not in the dict, create, add, go through 4
+         #1 -> in dict, add copy to current neighs
+         #3---> in dict, add copy to current neighs
+                                             
+         
+        
+        
+        
+        
+        
+        
