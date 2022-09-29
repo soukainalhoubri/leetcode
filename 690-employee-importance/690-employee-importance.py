780d2b4e5b07d563809dc9f1886fced06a7b7239
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-09-29 11:30:51 +0100

    Time: 310 ms (28.94%), Space: 15.5 MB (62.11%) - LeetHub

--- 690-employee-importance/690-employee-importance.py
+++ 690-employee-importance/690-employee-importance.py
@@ -6,28 +6,18 @@ class Employee:
         self.importance = importance
         self.subordinates = subordinates
 """
-
 from collections import deque
-
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        
-        #we first need to store the items in a way to make access to them easier by ID
-        #we will use a dictionnary: creating an adjaceny list
-        
-        importance={i.id:[i.importance,i.subordinates] for i in employees}#O(V) T&S
-        q=deque()#O(V) 
-        q.append(id)
-        answer=0#O(1) 
+        ids={employee.id:employee for employee in employees}
+        importance=0
+        q=deque()
+        q.append(ids[id])
         while q:
             current=q.popleft()
-            answer+=importance[current][0]
-            for i in importance[current][1]:
-                q.append(i)
-            
-        return answer
-        
-
-
-
+            importance+=current.importance
+            for sub in current.subordinates:
+                q.append(ids[sub])
+                
+        return importance
             
