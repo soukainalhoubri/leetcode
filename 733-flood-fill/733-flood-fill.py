43cdb37a88e41980a8e917a27a37844f0bbd8b9c
soukaina lhoubri <74101506+soukainalhoubri@users.noreply.github.com>
2022-09-29 01:51:14 +0100

    Time: 85 ms (86.28%), Space: 14 MB (89.71%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,15 +1,21 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        R=len(image)
-        C=len(image[0])
-        original=image[sr][sc]
-        def dfs(r,c):
-            if(not(0<=r<R and 0<=c<C))or image[r][c]!=original:
-                return
-            image[r][c]=color
-            [dfs(r+x,c+y) for (x,y) in [(1,0),(-1,0),(0,1),(0,-1)]]
-        if original!=color:
-            dfs(sr,sc)
+        #current_color need to be saved
+        #some sort of recursion here
+        initial_color=image[sr][sc]
+        n=len(image)
+        m=len(image[0])
+        if color==initial_color:
+            return image
+        def recursion(image, sr, sc):
+            #deal with the neighbors of the current cell
+            image[sr][sc]=color
+            neighbors=[[sr-1,sc],[sr+1,sc],[sr,sc-1],[sr,sc+1]]
+            for adj in neighbors:
+                if 0<=adj[0]<n and 0<=adj[1]<m and image[adj[0]][adj[1]]==initial_color:
+                    recursion(image, adj[0], adj[1])
+            return
+        recursion(image,sr,sc)
+        
         return image
-    
-    
+        
